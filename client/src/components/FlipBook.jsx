6848868826777817a054373209447f4c25cddbbe
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-12-12 16:28:25 -0500

    built FlipBook component

--- client/src/components/FlipBook.jsx
+++ client/src/components/FlipBook.jsx
@@ -1,10 +1,48 @@
-
+import '../css/component-css/FlipBook.scss';
 
 
 const FlipBook = () => {
   return (
-    <div>
+    <div className="flipbook-div">
+        <div class="imgLoader"></div>
 
+<div class="container">
+
+  <h1 class="title">
+    Where Writers Who Wander<br />are not lost
+  </h1>
+
+{/* <!--   <div class="credit">
+    * Images loaded randomly from Picsum.photos
+  </div> --> */}
+
+  <div class="book">
+    <div class="gap"></div>
+    <div class="pages">
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
+    </div>
+    <div class="flips">
+      <div class="flip flip1">
+        <div class="flip flip2">
+          <div class="flip flip3">
+            <div class="flip flip4">
+              <div class="flip flip5">
+                <div class="flip flip6">
+                  <div class="flip flip7"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -2,11 +2,13 @@ import TomeLibrary from "./TomeLibrary";
 import { Link } from "react-router-dom";
 import SignIn from "./SignIn";
 import '../css/screens-css/Home.css';
+import FlipBook from "../components/FlipBook";
 
 const Home = ({ posts }) => {
   return (
     <div>
       <div className="nav-filler"></div>
+      <FlipBook />
   </div>
   )
 }
