475d6dd373457bc9d63a83cd34b7ebf648f34ad1
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-12-13 11:31:28 -0500

    implemented InfoCards & styled. desktop mid of home page done. Queries needed

--- client/src/components/InfoCards.jsx
+++ client/src/components/InfoCards.jsx
@@ -1,17 +1,25 @@
 import lovecraft from '../assets/lovecraft.png';
 import poe from '../assets/poe.png';
+import alice from '../assets/alice.png';
+import border from '../assets/border.png';
 import '../css/component-css/InfoCards.css';
 
 
 const InfoCards = () => {
   return (
     <div className="info-card-main">
-      <div className="card1"><img src={lovecraft} /></div>
+      {/* <div className="card1"><img src={lovecraft} /></div> */}
       <div className="main-card">
+        <div className="border-container">
+          <img className="border-class" src={border} />
+        </div>
+        <div className="info-container">
         <h4 className="main-title">What is Nevermore?</h4>
         <p className="main-desc">Never more is in need of some information in this location. If you have any information or know where to find some, please list it out below where there is nowhere to list it. If you have problems listing it below, then please make sure to use the contact form in the links above that we have not provided.</p>
-      </div>
-      <div className="card2"><img src={poe}/></div>
+        <img className="alice" src={alice} />
+        </div>
+        </div>
+      <div className="card2"><img className="poe" src={poe}/></div>
     </div>
   )
 }
