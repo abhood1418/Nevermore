3b8519f8d29dfe75bf62eba3226236ce6df39e6b
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-12-14 12:30:25 -0500

    styled titles and username placement for library. library animations set.

--- client/src/components/BookModel.jsx
+++ client/src/components/BookModel.jsx
@@ -7,13 +7,17 @@ const BookModel = ({ post }) => {
     <div className="library-main-div">
   <div class="book3">
   <div class="back"></div>
-  <div class="page6"></div>
+  <div class="page6"><h3 className="text-interior">Click to read more</h3></div>
   <div class="page5"></div>
   <div class="page4"></div>
   <div class="page3"></div>
   <div class="page2"></div>
   <div class="page1"></div>
-      <div class="front">{post.title}</div>
+        <div class="front">
+          <h1 className="library-post-title">{post.title}</h1>
+          <br />
+          <h2 className="library-post-user">Username with go here</h2>
+        </div>
       </div>
       </div>
   )
