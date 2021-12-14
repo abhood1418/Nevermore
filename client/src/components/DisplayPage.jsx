086e442391a23f96ddca9d3030f90b7d39a1fb79
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-12-14 16:29:59 -0500

    added more styling to Display Page

--- client/src/components/DisplayPage.jsx
+++ client/src/components/DisplayPage.jsx
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
+import "../css/component-css/DisplayPage.css";
 
 const DisplayPage = ({ onePost, currentUser }) => {
   console.log(currentUser);
   console.log(onePost);
   return (
-    <div>
-      <h1>{onePost.title}</h1>
-      {currentUser.id === onePost.user_id ? (
-        <Link to={`/library/${onePost.id}/edit`}>click me</Link>
-      ) : null}
+    <div className="displaypage-master-div">
+      <div className="content-page">
+        <h1 className="display-title">{onePost.title}</h1>
+        <div className="content-wrapper">
+          <p className="display-content">{onePost.content}</p>
+        </div>
+        {currentUser.id === onePost.user_id ? (
+          <Link className="edit-link" to={`/library/${onePost.id}/edit`}>
+            click me
+          </Link>
+        ) : null}
+      </div>
     </div>
   );
 };
