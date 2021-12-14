ed0ed283a63660587b0bdd09707487f0a7598a1c
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-12-14 13:31:40 -0500

    updates for deployment

--- client/src/components/BookModel.jsx
+++ client/src/components/BookModel.jsx
@@ -1,26 +1,27 @@
-import '../css/component-css/BookModel.css';
-
+import "../css/component-css/BookModel.css";
 
 const BookModel = ({ post }) => {
   console.log(post);
   return (
     <div className="library-main-div">
-  <div class="book3">
-  <div class="back"></div>
-  <div class="page6"><h3 className="text-interior">Click to read more</h3></div>
-  <div class="page5"></div>
-  <div class="page4"></div>
-  <div class="page3"></div>
-  <div class="page2"></div>
-  <div class="page1"></div>
+      <div class="book3">
+        <div class="back"></div>
+        <div class="page6">
+          <h3 className="text-interior">Click to read more</h3>
+        </div>
+        <div class="page5"></div>
+        <div class="page4"></div>
+        <div class="page3"></div>
+        <div class="page2"></div>
+        <div class="page1"></div>
         <div class="front">
           <h1 className="library-post-title">{post.title}</h1>
           <br />
           <h2 className="library-post-user">Username with go here</h2>
         </div>
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
 export default BookModel;
