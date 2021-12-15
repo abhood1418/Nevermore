5ea73f3dd680dc4c3ffecc8ee274afd46bc74b8a
Andrew Hood <andrewhood@Andrews-MacBook-Pro.local>
2021-12-15 11:22:39 -0500

    set footer styling. added git and linkedin links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,8 @@
 import logo from "../assets/logo.png";
-import '../css/component-css/Footer.css';
+import "../css/component-css/Footer.css";
+import git from "../assets/git.png";
+import linked from "../assets/linked.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -8,8 +11,16 @@ const Footer = () => {
         <img className="logo-footer" src={logo} />
         <h4 className="my-name">Created by ABHood</h4>
       </div>
+      <div className="contact">
+        <a href="https://github.com/abhood1418">
+          <img className="icon" src={git} />
+        </a>
+        <a href="https://www.linkedin.com/in/andrew-b-hood/">
+          <img className="icon" src={linked} />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
